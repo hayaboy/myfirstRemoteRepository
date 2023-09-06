87e3f5ae84f59aa75929f8c3f5242fc90be79756
hayaboy <hayaboy@korea.ac.kr>
2023-09-06 17:16:55 +0900

    리스트테스트_버전1_9_6_5시16분

---
+++ ListTest1.java
@@ -0,0 +1,33 @@
+package collection1;
+
+import java.util.ArrayList;
+
+public class ListTest1 {
+
+	public static void main(String[] args) {
+	
+		ArrayList<Double> arrLists = new ArrayList<Double>();
+		
+		System.out.println(arrLists);
+		
+		arrLists.add(10.4);
+		arrLists.add(10.4);
+		arrLists.add(10.4);
+		arrLists.add(10.4);
+		
+		System.out.println(arrLists);
+		
+		
+		double tot=0;
+		for(double arrList : arrLists) {
+			tot += arrList;
+		}
+		System.out.println(tot);
+		
+		
+		double avg=tot / arrLists.size();
+		System.out.println(avg);
+		
+	}
+
+}
